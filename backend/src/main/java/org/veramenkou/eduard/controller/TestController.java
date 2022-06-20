089fcde96366a462f6cf91ca32f3b19b8d29f9c2
EVeremenkov <hands.fins@gmail.com>
2022-06-20 22:50:55 +0300

    Create two modules with simple projects

---
+++ backend/src/main/java/org/veramenkou/eduard/controller/TestController.java
@@ -0,0 +1,13 @@
+package org.veramenkou.eduard.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    String get() {
+        return "HELLO!!!";
+    }
+}
